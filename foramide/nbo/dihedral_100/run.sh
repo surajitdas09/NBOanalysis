f8c1024eca13e9da7cd21ce7f3d96c71791fe3ba
Surajit Das <surajit@SurajitMac.local>
2026-07-20 12:28:00 +0530

    sd:full nbo

---
+++ foramide/nbo/dihedral_100/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -V
+#PBS -N dihedral_100_nbo
+#PBS -q workqa
+#PBS -l nodes=1:ncpus=14
+export OMP_NUM_THREADS=1
+ulimit -s unlimited
+export PATH=/apps/openmpi/openmpi-3.1.3_install/bin:$PATH
+export LD_LIBRARY_PATH=/apps/openmpi/openmpi-3.1.3_install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/apps/lib:$LD_LIBRARY_PATH
+WORKDIR=/scratch/surajit/PBS_$PBS_JOBID
+mkdir $WORKDIR
+cd $WORKDIR
+ORCA=/apps/orca/orca.6.0.1/orca
+export QCSCRATCH=$WORKDIR
+mydir=/home/surajit/nbo_analysis/formamide/nbo/dihedral_100
+cp $mydir/* .
+echo $HOSTNAME > scrpath.txt
+echo $PWD >> scrpath.txt
+cp scrpath.txt $mydir/
+$ORCA nbo.com > nbo.out
+cp * $mydir/
+rm -rf $WORKDIR
---
+++ foramide/nbo/dihedral_160/nbo.com
@@ -0,0 +1,3 @@
+! B3LYP cc-pVDZ def2/J RIJCOSX NBO
+
+* xyzfile 0 1 dihedral.xyz
